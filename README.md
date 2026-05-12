d9898f38dd35428b250aa50ea268bdc2ffacdef2
nosurfer <nosurfer@mail.ru>
2026-05-12 13:04:34 +0300

    docs: readme change

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # edbg
-# ujdybotttttt
+
+## Install
+```bash
+git clone https://github.com/nosurfer/edbg.git
+cd edbg
+make pull
+make
+./edbg
+```
+
+## Todo
+
+- [ ] more comments everywhere...
+- [ ] breakpoint handler
+- [ ] continue handler
+- [ ] step handler
+- [ ] getting registers info
+- [ ] info maps (vmmap)
+- [ ] next handler
+- [ ] memmory read / write
+- [ ] process status handling
+- [ ] help and quit features
+- [ ] tests
+
+### Done
+- [x] add logs (extern lib)
+- [x] temporary makefile
--- src/main.cc
+++ src/main.cc
@@ -11,22 +11,17 @@ main(int argc, char *argv[])
 {
   spdlog::cfg::load_env_levels();
 
-  SPDLOG_TRACE("Entering main()");
-  SPDLOG_DEBUG("argc = {}", argc);
-  SPDLOG_INFO("Program started");
-  SPDLOG_WARN("No config file found, using defaults");
-  SPDLOG_ERROR("Failed to open file: {}", "data.txt");
-  SPDLOG_CRITICAL("Fatal error, exiting");
+  SPDLOG_TRACE("Program start");
   
   if (argc < 2) {
     std::cerr << "Usage: " << argv[0] << " <pid>" << std::endl;
   }
 
   /* todo
-   * 1. add logs (extern lib)
-   * 2. more comments everywhere...
-   * 3. breakpoint handler
-   * 4. temporary makefile */
+   * [x] add logs (extern lib)
+   * [ ] more comments everywhere...
+   * [ ] breakpoint handler
+   * [ ] temporary makefile */
   
   return 0;
 }
